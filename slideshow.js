c4c34d95b7e6e45a4fbdecec97ae422dbc7dbe50
Helge Rausch <helge@rausch.io>
2011-03-09 18:35:48 +0100

    fixing indentation

--- slideshow.js
+++ slideshow.js
@@ -3,17 +3,17 @@
  *
  *
  * Copyright (C) 2011 by Helge Rausch <helge@rausch.io>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -45,9 +45,9 @@ $(function() {
 
       link.click(function(event) {
         navigation.find('a.current_slide').removeClass('current_slide');
-	$(event.currentTarget).addClass('current_slide');
-	carrier.animate({left: (i * -100 + '%')}, 400);
-	return false;
+        $(event.currentTarget).addClass('current_slide');
+        carrier.animate({left: (i * -100 + '%')}, 400);
+        return false;
       });
     });
   });
