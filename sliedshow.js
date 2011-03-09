bdb0250d02ab292eac72affbdde5cbcb33228fe3
Helge Rausch <helge@rausch.io>
2011-03-09 14:56:37 +0100

    Oh look! It's a slide show!

---
+++ sliedshow.js
@@ -0,0 +1,53 @@
+/*
+ * Leaf Peeper - a jQuery based slide show
+ *
+ *
+ * Copyright (C) 2011 by Helge Rausch <helge@rausch.io>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+// A demo of the different easing functions can be found here:
+// http://gsgd.co.uk/sandbox/jquery/easing/
+$.easing.def = "easeOutQuad";
+
+$(function() {
+  $('div.slideshow').each(function() {
+    var slideshow = $(this);
+    var carrier = slideshow.find('div.carrier').first();
+    var slides = slideshow.find('div.slide')
+    var navigation = slideshow.find('div.navigation')
+    carrier.css({width: (slides.size() * 100 + '%')});
+
+    slides.each(function(i) {
+      var link_text = $(this).attr('data-name') || (i+1);
+      var link = $('<a href="#">' + link_text + '</a>');
+      navigation.append(link);
+      if(i == 0)
+        link.addClass('current_slide');
+
+      link.click(function(event) {
+        navigation.find('a.current_slide').removeClass('current_slide');
+	$(event.currentTarget).addClass('current_slide');
+	carrier.animate({left: (i * -100 + '%')}, 400);
+      });
+    });
+  });
+});
